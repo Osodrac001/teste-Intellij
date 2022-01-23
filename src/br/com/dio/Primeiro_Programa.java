8bca2e785c99db6b9e9ccac3fb33acafc6a5f949
Osodrac <alcsantosfn@gmail.com>
2022-01-22 23:11:06 -0300

    Primeiro commit

---
+++ src/br/com/dio/Primeiro_Programa.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class Primeiro_Programa {
+
+    public static void main(String[] args) {
+      Gato gato = new Gato();
+
+      System.out.println(gato);
+
+
+       /* int a = 8;
+        int b = 9;
+
+        System.out.println("Hello World " + (a+b));*/
+    }
+
+
+
+
+
+
+}
